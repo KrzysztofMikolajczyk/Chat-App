cf61c07ac43b452f34c2271d81412ad81a61e670
KrzysztofMikolajczyk <krzysztof.mikolajczyk@hotmail.com>
2019-12-12 19:25:34 +0100

    Add Chat Users + their atributes, changes in layout, add online/offline dependencies and components

--- smackchat/src/pages/PageUsers.vue
+++ smackchat/src/pages/PageUsers.vue
@@ -1,12 +1,53 @@
 <template>
-  <q-page class="flex">
-  <h5>Panel użytkownika</h5>
+  <q-page class="flex q-pa-md">
+      <q-list class="full-width" separator >
+      <q-item 
+      v-for="user in users" 
+      :key="user.id" 
+      to="/chat"
+      clickable 
+      v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge 
+            :color="user.online ? 'green-5' : 'grey-6'">
+            {{ user.online ? 'Online' : 'Offline' }}
+          </q-badge>
+        </q-item-section> 
+      </q-item>  
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+    users: [{
+    id: 1,
+    name: 'Krzysztof Mikołajczyk',
+    online: true,
+    }, {
+    id: 2,
+    name: 'Mateusz Arendarski',
+    online: false,
+    }, {
+    id: 3,
+    name: 'Nadia Marat',
+    online: true,
+    }]
+    }
+  }
 }
 </script>
 
