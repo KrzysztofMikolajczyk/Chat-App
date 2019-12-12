f597d4fb3b4a362669677f1213a8610da157788c
KrzysztofMikolajczyk <krzysztof.mikolajczyk@hotmail.com>
2019-12-12 18:09:55 +0100

    Install environment & Create project, pages and route + basics layout

---
+++ smackchat/src/layouts/MyLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+      
+
+        <q-toolbar-title class="absolute-center">
+          {{ title }}
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  computed: {
+    title() {
+      console.log(this.$route)
+      let currentPath = this.$route.fullPath
+      if (currentPath == '/') return 'Chat App'
+      else if (currentPath == '/chat') return 'Chat'
+      else if (currentPath == '/auth') return 'Login'
+    }
+  }
+}
+</script>
